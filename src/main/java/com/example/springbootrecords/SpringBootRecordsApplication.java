5c563f28597c5e6c823cc314ef48bb8e97e7e5c4
J2RGEZ <j2rgez@gmail.com>
2018-03-05 11:34:54 +0000

    Basic setup

---
+++ src/main/java/com/example/springbootrecords/SpringBootRecordsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecordsApplication.class, args);
+	}
+}
