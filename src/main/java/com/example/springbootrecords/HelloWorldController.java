a2a6478c75719a5e91b69329e39b8b360882ca68
JuanRaBetancor <jr-ulpgc@hotmail.com>
2018-03-08 12:32:06 +0000

    commit preparing JR project

--- src/main/java/com/example/springbootrecords/HelloWorldController.java
+++ src/main/java/com/example/springbootrecords/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")
     public String sayHello(){
-        return "Hello!";
+        return "Helloo!";
     }
 }
