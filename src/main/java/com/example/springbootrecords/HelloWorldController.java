cef919d4cee14703c146f9d36cd73604e0e21d61
JuanRaBetancor <jr-ulpgc@hotmail.com>
2018-03-15 12:22:02 +0000

    simple index view

--- src/main/java/com/example/springbootrecords/HelloWorldController.java
+++ src/main/java/com/example/springbootrecords/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.example.springbootrecords;
 
+import com.example.springbootrecords.history.model.HistoryEntry;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/")
 public class HelloWorldController {
-    @RequestMapping("/")
-    public String sayHello(){
-        return "Helloo!";
+
+    @GetMapping()
+    public ModelAndView getHistory(){
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
     }
 }
